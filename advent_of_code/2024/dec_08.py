9966569164a9fe3bcfb0319fb788799d2df8eef5
mheguy <10162554+mheguy@users.noreply.github.com>
2025-02-16 14:48:36 -0500

    Dec_08 p2

--- advent_of_code/2024/dec_08.py
+++ advent_of_code/2024/dec_08.py
@@ -14,6 +14,9 @@ class Position:
     def __add__(self, other: "Position") -> "Position":
         return Position(self.x + other.x, self.y + other.y)
 
+    def __mul__(self, other: int) -> "Position":
+        return Position(self.x * other, self.y * other)
+
 
 @dataclass(frozen=True)
 class Tower:
@@ -21,31 +24,50 @@ class Tower:
     frequency: str
 
 
+class Grid:
+    def __init__(self, lines: Lines):
+        self.x_min = 0
+        self.y_min = 0
+        self.x_max = len(lines[0]) - 1
+        self.y_max = len(lines) - 1
+        self.largest_axis = max(self.x_max, self.y_max)
+
+    def __contains__(self, pos: Position) -> bool:
+        return pos.x >= self.x_min and pos.x <= self.x_max and pos.y >= self.y_min and pos.y <= self.y_max
+
+
 def main(lines: Lines) -> None:
-    results = get_result(lines)
-    print(results)
+    print(get_result(lines))
 
 
-def get_result(lines: Lines) -> int:
+def get_result(lines: Lines) -> tuple[int, int]:
+    grid = Grid(lines)
     towers = get_towers(lines)
     frequencies = {t.frequency for t in towers}
 
-    antinodes: list[Position] = []
+    p1_antinodes: list[Position] = []
+    p2_antinodes: list[Position] = []
     for frequency in frequencies:
         towers_with_frequency = [t for t in towers if t.frequency == frequency]
         for first, second in itertools.permutations(towers_with_frequency, 2):
+            p2_antinodes.extend([first.pos, second.pos])
             offset = Position(first.pos.x - second.pos.x, first.pos.y - second.pos.y)
 
-            antinodes.append(first.pos + offset)
+            antinode = first.pos + offset
 
-    # Remove antinodes that are outside of grid
-    x_min = 0
-    y_min = 0
-    x_max = len(lines[0]) - 1
-    y_max = len(lines) - 1
-    antinodes = [a for a in antinodes if a.x >= x_min and a.x <= x_max and a.y >= y_min and a.y <= y_max]
+            if antinode not in grid:
+                continue
 
-    return len(set(antinodes))
+            p1_antinodes.append(antinode)
+
+            counter = 1
+            while antinode in grid:
+                p2_antinodes.append(antinode)
+
+                counter += 1
+                antinode = first.pos + offset * counter
+
+    return len(set(p1_antinodes)), len(set(p2_antinodes))
 
 
 def get_towers(lines: Lines) -> list[Tower]:
